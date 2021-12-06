3f71c6534b6d93229581955a65c2d821332463c6
Christian <cespinoza8001@gmail.com>
2021-12-06 18:04:53 -0500

    added search bar and some post styling

---
+++ my-app-frontend/src/components/FavoritesPage.js
@@ -0,0 +1,13 @@
+import PostContainer from "./PostContainer"
+
+function FavoritesPage() {
+    return (
+      <div>
+        <header>
+        </header>
+        <PostContainer />
+      </div>
+    );
+  }
+  
+  export default FavoritesPage;
--- my-app-frontend/src/components/Post.js
+++ my-app-frontend/src/components/Post.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from 'react'
+import '../Post.css'
 
 function Post({post:{id, header, description, img, content, likes, user_id}}) {
     
@@ -11,16 +12,44 @@ function Post({post:{id, header, description, img, content, likes, user_id}}) {
     }, []) 
     
     return (
-        <div>
-            <h2>{header}</h2>
-            <h3>{description}</h3>
-            <img src={img}/>
-            <p>By: {username}</p>
-            <p>{content}</p>
-            <p>Likes: {likes}</p>
-            <p>Comments:</p>
+        // <div class="mock-outer">
+        //     <h2 class="fbh5">{header}</h2>
+        //     <h3>{description}</h3>
+        //     <img src={img}/>
+        //     <p>By: {username}</p>
+        //     <p>{content}</p>
+        //     <p>Likes: {likes}</p>
+        //     <p>Comments:</p>
+        // </div>
+
+        <div class="mock-outer">
+		<div class="mock-inner">
+        <div class="fb-group-picrow">  
+            <img src={img} />
+                <div class="fb-group-text-top">  <div class="fb-group-text"> 
+                    <h5 class="fbh5">{username}</h5>
+                    <span class="fb-group-date">Right Now</span>
+                    </div>
+                    </div>
+            </div>   
+		<div class="usertext"><p>{header}</p></div>
+		<div class="mock-img-all">
+		
+		<div class="mock-img"></div>
+		<div class="mock-title">
+		<div class="mock-title2">
+		<div class="mock-title-top">
+		Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title Title
+		</div>
+		<div class="mock-title-mid">
+		Description:{content}</div>
+		</div>
+		</div>
+		</div>
+        <p>Likes:{likes}</p>
+		</div>
         </div>
     )
 }
 
-export default Post
+export default Post;
