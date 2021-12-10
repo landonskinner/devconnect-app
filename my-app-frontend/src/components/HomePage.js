b59ce92746bfe179b287424f468fa43fd8542b62
Michael Toth <mtoth003@gmail.com>
2021-12-10 10:42:21 -0500

    added home styling

--- my-app-frontend/src/components/HomePage.js
+++ my-app-frontend/src/components/HomePage.js
@@ -1,6 +1,6 @@
 import PostContainer from "./PostContainer";
 import {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
+import NavBar from "./NavBar"
 
 
 function HomePage({search, loginId}){
@@ -10,7 +10,10 @@ function HomePage({search, loginId}){
   useEffect(() => {
     fetch(`http://localhost:9292/users/${loginId}`)
     .then(res => res.json())
-    .then(data => setUserData(data)
+    .then(data => {
+      setUserData(data)
+      // window.location.reload(false)
+    }
   )}, [loginId])
 
   return(
@@ -19,10 +22,11 @@ function HomePage({search, loginId}){
       <div className="home-title">
         <h2 className="page-title">Dev<em>Connect</em></h2>
       </div>
+      <div className="home-nav">
+        <NavBar/>
+      </div>
       <div>
-
         <img className="prof-photo-home" src={userData[0].image_url} />
-        <Link to="/account" className="account-link">My Account</Link>
       </div>
       <div className="home-posts">
         <PostContainer search={search} page="home" loginId={loginId}/>
